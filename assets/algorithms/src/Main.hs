3ccb873509c7339895c74a2f872047016a26e6df
Victor Palade <victor@cloudflavor.io>
2018-08-26 23:36:28 +0200

    *: add basig algoritm attempt

--- assets/algorithms/src/Main.hs
+++ assets/algorithms/src/Main.hs
@@ -1,9 +1,21 @@
 module Main where
 
+
 main :: IO ()
 main = do
-  print $ generateDataSet 1000
+  print $ insertionSort $ generateDataSet 1000
 
-
+-- Generate a dataset to be used in sorting.
+-- We can pass the size of the array as a parameter.
 generateDataSet :: Int -> [Int]
 generateDataSet size = take size [0..]
+
+-- An implementation of a simple insertion sort algorithm.
+insertionSort :: [Int] -> [Int]
+insertionSort dataSet = do
+  j <- newArray n dataSet
+  return j
+  where
+    n = length dataSet
+    newArray :: Int -> [Int]
+    newArray = [0]
